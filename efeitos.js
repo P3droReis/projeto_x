98b521bc9df89aee8af4fccde376a93b15407d06
Pedro Reis <126337464+P3droReis@users.noreply.github.com>
2025-01-13 20:36:00 -0300

    atualização de efeitos

--- efeitos.js
+++ efeitos.js
@@ -1,5 +1,11 @@
 window.aparecer = ScrollReveal({reset:true});
 
 aparecer.reveal('.efeito-topo',{
-    duration: 3000,
-})
+    duration: 4500
+});
+
+aparecer.reveal('.efeito-sobre-nos',{
+    duration: 2000,
+    distance: '100px'
+});
+
